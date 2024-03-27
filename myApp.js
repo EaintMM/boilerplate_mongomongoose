7822ab987a68283dd05ec82652428adc64a7d56b
Eaint MM <eaintmyintmon@gmail.com>
2024-03-27 15:37:10 +0000

    Mongoose shcema and model

--- myApp.js
+++ myApp.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 let mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// Person Schema
+let personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
 
 let Person;
+
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
